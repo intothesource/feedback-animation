e67b1b01622bc241da707ebe9d545a80cd9547e2
Ramon Valk <rvalk@intothesource.com>
2019-10-21 14:45:15 +0200

    Finished functionality

--- feedback-animation.js
+++ feedback-animation.js
@@ -4,28 +4,40 @@ const elements = document.querySelectorAll(`[${DATA_ELEMENT}]`);
 
 const animateElementRipple = (element, color) => {
     element.onmousedown = e => {
+        let mouseUp = false;
+        const mouseUpListener = document.onmouseup = () => {
+            mouseUp = true;
+            element.removeEventListener('mouseup', mouseUpListener);
+        }
 
         const x = e.pageX - element.offsetLeft;
         const y = e.pageY - element.offsetTop;
-        const w = element.offsetWidth;
-        
+        const w = element.offsetWidth * 1.2;
+
         const ripple = document.createElement('span');
-        
+
         ripple.className = 'ripple';
         ripple.style.left = x + 'px';
-        ripple.style.top  = y + 'px';
+        ripple.style.top = y + 'px';
         ripple.style.setProperty('--scale', w);
 
         if (element.dataset.itsFeedbackColor) {
             ripple.style.setProperty('--color', color);
         }
-    
-        element.appendChild(ripple);
-    
+
+        element.prepend(ripple);
+
         setTimeout(() => {
-          ripple.parentNode.removeChild(ripple);
-        }, 500);
-      }
+            if (mouseUp) {
+                ripple.parentNode.removeChild(ripple);
+            } else {
+                const mouseUpListener = document.onmouseup = () => {
+                    ripple && ripple.parentNode.removeChild(ripple);
+                    element.removeEventListener('mouseup', mouseUpListener);
+                }
+            }
+        }, 300);
+    }
 }
 
 if (elements) {
